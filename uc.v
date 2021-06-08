e3afc6705515d6a0ed49443f828cab9790a1ead4
Ángel <alu0101139456@ull.edu.es>
2021-06-08 19:33:00 +0100

    Simplificamos la asignación de señales usando un regsuitro

--- uc.v
+++ uc.v
@@ -13,36 +13,40 @@ parameter NOJUMP  = 4'b1000;
 parameter NOP     = 4'b0000;
 reg [3:0] operation;
 
+reg [3:0] signals; // ver si hace falta inicializarlo
+
+assign {s_inc, s_inm, we3, wez} = signals;
+
 always @(opcode) begin
     op_alu = opcode[4:2];
     casez (opcode)
         6'b0?????: // operacion aritmetica
-            {s_inc, s_inm, we3, wez} = ARITH; 
+            signals = ARITH; 
 
         6'b1000??: // carga de inmediato
-            {s_inc, s_inm, we3, wez} = LOADINM;
+            signals = LOADINM;
 
         6'b10010?: // salto condicional
             begin
                 if (opcode[0] == 0) // branch equal zero
                     if(z)
-                        {s_inc, s_inm, we3, wez} = JUMP;
+                        signals = JUMP;
                     else
-                        {s_inc, s_inm, we3, wez} = NOJUMP;
+                        signals = NOJUMP;
 
                 else  // branch not iqual zero
                     if(z)
-                        {s_inc, s_inm, we3, wez} = NOJUMP;
+                        signals = NOJUMP;
                     else
-                        {s_inc, s_inm, we3, wez} = JUMP;
+                        signals = JUMP;
             end
 
         6'b100110: // salto incondicional
-            {s_inc, s_inm, we3, wez} = JUMP;
+            signals = JUMP;
         
         
         default: 
-            {s_inc, s_inm, we3, wez} = NOP; 
+            signals = NOP; 
     endcase
     
 end
