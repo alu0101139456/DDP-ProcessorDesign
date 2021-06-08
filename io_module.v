ac7cc6dc7b916234f0a417d9fa20c7f1191f13ba
Ángel <alu0101139456@ull.edu.es>
2021-06-08 19:37:37 +0100

    Cambiado nombre

--- io_module.v
+++ io_module.v
@@ -1,6 +1,7 @@
-module io_module ( input wire clk, reset, sel_port, we, input wire [7:0] in_p0, in_p1, in_RD2, output wire [7:0] out_p0, out_p1, out );
+module io_module ( input wire clk, reset, sel_port, we, input wire [7:0] in_p0, in_p1, in_RD2, output wire [7:0] out_p0, out_p1, in_port_out );
 
-    input_module IN_MODULE(clk, reset, sel_port, in_p0, in_p1, out);
+   
+    input_module IN_MODULE(clk, reset, sel_port, in_p0, in_p1, in_port_out);
 
     output_module OUT_MODULE(clk, reset, sel_port, we, in_RD2, out_p0, out_p1);
 
