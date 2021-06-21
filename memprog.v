002d05b7ab305a5a33999b8296e3168a14713e87
Ángel <alu0101139456@ull.edu.es>
2021-06-21 18:57:59 +0100

    Añadido memstack para pila con we y data_in data_out

--- memprog.v
+++ memprog.v
@@ -15,4 +15,19 @@ module memprog #(parameter WIDTH = 16, parameter ELEMENTOS = 1024)(input  wire  
 
 endmodule
 
+//Memoria de programa, se inicializa y no se modifica
 
+module memstack #(parameter WIDTH = 16, parameter ELEMENTOS = 1024)(input  wire clk, we,
+               input  wire [$clog2(ELEMENTOS)-1:0]  a,
+               input  wire [WIDTH - 1:0] data_in,
+               output wire [WIDTH - 1:0] data_out);
+
+  reg [WIDTH - 1:0] mem[0:ELEMENTOS-1]; //memoria de 1024 palabras de 16 bits de ancho
+
+ always @(posedge clk)
+    if (we) mem[a] <= data_in;	
+  
+  assign data_out = mem[a];
+
+  
+endmodule
