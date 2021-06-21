1c4e01f5bd5a3c3fe86d68dcc90f233697d43c03
Ángel <alu0101139456@ull.edu.es>
2021-06-21 18:59:04 +0100

    Creado modulo stack

---
+++ stack_module.v
@@ -0,0 +1,11 @@
+module stack_module (input wire  clk, reset, we_stack, s_jalret, input wire [9:0] data_in, output wire [9:0] data_out );
+
+
+wire [3:0] out_sp, out_sum, out_mux;
+
+sum #(4) SUMADOR(out_sp, out_mux, out_sum);
+mux2 #(4) MUX_TO_INCDEC(4'b0001,4'b1111, s_jalret, out_mux); 
+registro #(4) STACK_POINTER(clk, reset, out_sum, out_sp);
+memstack #(10,16) MEM_STACK(clk, we_stack, out_sp, data_in, data_out);
+
+endmodule
