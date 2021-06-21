015dc2c1d62b661039909b4f716b69d4f23b8175
Ángel <alu0101139456@ull.edu.es>
2021-06-21 18:58:32 +0100

    modificado sum para parametrizarlo

--- componentes.v
+++ componentes.v
@@ -27,8 +27,8 @@ module regfile(input  wire        clk,
 endmodule
 
 //modulo sumador  
-module sum(input  wire [9:0] a, b,
-             output wire [9:0] y);
+module sum #(parameter WIDTH = 10) (input  wire [WIDTH - 1:0] a, b,
+             output wire [WIDTH - 1:0] y);
 
   assign y = a + b;
 
