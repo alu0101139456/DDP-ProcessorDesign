3fe544d595a12f9c0b0a256f54e031e5de66e4cf
Ángel <alu0101139456@ull.edu.es>
2021-07-18 13:00:16 +0100

    Modificada pila para ser parametrizada

--- memprog.v
+++ memprog.v
@@ -1,10 +1,10 @@
 //Memoria de programa, se inicializa y no se modifica
 
-module memprog #(parameter WIDTH = 16, parameter ELEMENTOS = 1024)(input  wire        clk,
-               input  wire [$clog2(ELEMENTOS)-1:0]  a,
+module memprog #(parameter WIDTH = 16, parameter NWORDS = 1024)(input  wire        clk,
+               input  wire [$clog2(NWORDS)-1:0]  a,
                output wire [WIDTH - 1:0] rd);
 
-  reg [WIDTH - 1:0] mem[0:ELEMENTOS-1]; //memoria de 1024 palabras de 16 bits de ancho
+  reg [WIDTH - 1:0] mem[0:NWORDS-1]; //memoria de 1024 palabras de 16 bits de ancho
 
   initial
   begin
@@ -17,12 +17,12 @@ endmodule
 
 //Memoria de programa, se inicializa y no se modifica
 
-module memstack #(parameter WIDTH = 16, parameter ELEMENTOS = 1024)(input  wire clk, we,
-               input  wire [$clog2(ELEMENTOS)-1:0]  a,
+module memstack #(parameter WIDTH = 16, parameter NWORDS = 1024)(input  wire clk, we,
+               input  wire [$clog2(NWORDS)-1:0]  a,
                input  wire [WIDTH - 1:0] data_in,
                output wire [WIDTH - 1:0] data_out);
 
-  reg [WIDTH - 1:0] mem[0:ELEMENTOS-1]; //memoria de 1024 palabras de 16 bits de ancho
+  reg [WIDTH - 1:0] mem[0:NWORDS-1]; //memoria de 1024 palabras de 16 bits de ancho
 
  always @(posedge clk)
     if (we) mem[a] <= data_in;	
